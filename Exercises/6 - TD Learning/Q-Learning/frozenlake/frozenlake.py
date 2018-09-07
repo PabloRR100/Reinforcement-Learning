4784eea75a76a9a57e6c55c6675e97e6302f2396
PabloRR100 <pabloruizruiz10@gmail.com>
2018-09-07 08:53:56 -0400

    running - now implemente Q learning alg

--- Exercises/6 - TD Learning/Q-Learning/frozenlake/frozenlake.py
+++ Exercises/6 - TD Learning/Q-Learning/frozenlake/frozenlake.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 #import plotly.plotly as py
 #import plotly.offline as pyo
-import plotly.graph_objs as go
+#import plotly.graph_objs as go
 import plotly.figure_factory as ff
 
 import dash
@@ -13,25 +13,22 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 app = dash.Dash()
-app.css.append_css({
-    "external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"
-})
-
-
 
 # Environment
 
 import gym
 env = gym.make('FrozenLake-v0')
 
+EPISODES = 2000
 total_states = env.observation_space.n
 actions_per_state = env.action_space.n
 print('Number of states: ', total_states)
 print('Number of actions per state: ', actions_per_state)
+print('Number of episodes to run: ', EPISODES)
 
 
 
-# Q - Table
+# Q-Table
 
 q = np.zeros((total_states, actions_per_state), dtype='int')
 Q = pd.DataFrame(q, columns=['Left', 'Down', 'Right', 'Up'])
@@ -39,21 +36,63 @@ Q.index = range(1,len(q)+1)
 Q.reset_index(inplace=True)
 Q.rename(columns={'index':'state'}, inplace=True)
 
-table = ff.create_table(Q,  height_constant=20)
-table.layout.width=300
+# Helper functions to draw and update values of the table
+def draw_Table(Q):
+    table = ff.create_table(Q, index=True)
+    table.layout.height, table.layout.width=350, 350
+    for i in range(len(table.layout.annotations)):
+        table.layout.annotations[i].align = 'center'
+    return table
 
 def update_Q(Q):
     for i in range(len(Q)):
         for j in range(1, len(Q.columns)):        
             Q.iloc[i,j] += np.random.choice([0,1,2])
-    print(Q)
     return Q
+
+
+
+## Q-Learning Algorithm
+#
+#lr = .8
+#y = .9
+#rList = []
+#
+#for i in range(EPISODES):
+#    
+#    s = env.reset()
+#    
+#    rAll = 0
+#    end_state = False
+#    j = 0
+#    
+#    while j < 99:
+#        
+#        j+=1
+#        
+#        # Choose action greedily
+#        a = np.argmax(Q[s,:] + np.random.randn(1, env.action_space.n) * (1./ (i+1)))
+#        
+#        # Collect reward and reach new state
+#        s1,r,d,_ = env.step(a)
+#        
+#        # Update Q-Table with new knowledge
+#        Q[s,a] = Q[s,a] + lr*(r + y*np.max(Q[s1,:]) - Q[s,a])
+#        
+#        rAll += r
+#        s = s1
+#        
+#        if end_state == True: break
+#
+#    rList.append(rAll)
+#
+
 
 # Browser visualization
 
 app.layout = html.Div([
         html.H1(children='Frozen Lake: Q-Learning Demo'),
-        dcc.Graph(id='table', figure=table),
+        dcc.Graph(id='table', figure=draw_Table(Q)),
         dcc.Interval(
             id='time',
             interval=1*1000, # in milliseconds
@@ -66,8 +105,8 @@ app.layout = html.Div([
               [Input(component_id = 'time', component_property='n_intervals')])    
 def update_table(n):   
     # Update values
-    new_table = ff.create_table(update_Q(Q))
-    time.sleep(1)
+    new_table = draw_Table(update_Q(Q))
+    time.sleep(1 * np.exp(-n))
     return new_table
     
 
