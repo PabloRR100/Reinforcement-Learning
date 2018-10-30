c5c9324be5f8ae8b0e56ee097a3c1652ef03d5fd
PabloRR100 <pabloruizruiz10@gmail.com>
2018-10-30 08:17:47 -0400

    pong game

--- 2_Deep_RL/2_Policy_Based_Methods/2_Proximal_Policy_Optimization/pong/agent.py
+++ 2_Deep_RL/2_Policy_Based_Methods/2_Proximal_Policy_Optimization/pong/agent.py
@@ -4,8 +4,6 @@
 AI Agent that learns to play pong with Policy Gradient Optimizaion
 """
 
-import numpy as np
-
 # Define the Policy Network
 # -------------------------
 '''
@@ -15,40 +13,102 @@ at each frame and returns the probability distribution of taking the actions:
 * Actually it takes the difference between 2 frames, to detect motion
 '''
 
+# Numpy Implementation
+import numpy as np
+
 class Agent():
     
-    def __init__(self, input, hid):
+    def __init__(self, input, hid, gam=0.99, eps=1e-5, lr=1e-4, dw=0.99):
+        # Weights
         self.W1 = np.random.rand(input,hid)
-        self.W2 = np.random.rand(hid,1)
+        self.W2 = np.random.rand(hid,1)        
+        self.layers = {'W1': self.W1, 
+                       'W2': self.W2}
         
+        # Training parameters
+        self.gamma = gam        
+        self.epsilon = eps
+        self.learning_rate = lr
+        self.decay_weight = dw
+        
+        # Gradient memory
+        self.exp_g2 = {}
+        self.g_dict = {}
+        
+    def restart_gradients(self):
+        for layer_name in self.layers.keys():
+            self.exp_g2[layer_name] = np.zeros_like(self.layers[layer_name])
+            self.g_dict[layer_name] = np.zeros_like(self.layers[layer_name])
+            
+    def aggregate_gradients(self, grad):
+        for layer_name in grad:
+            self.g_dict[layer_name] += grad[layer_name]        
+
     def sigmoid(self, x):
         return 1.0 / (1.0 + np.exp(-x))
     
-    def prepro(self, I):
+    def preprocess(self, I):
         ''' Image preprocessing before entering the network '''
-        I = I[35:195]     # crop
-        I = I[::2,::2,0]  # downsample
-        I[I == 144] = 0   # erase background (background type 1)
-        I[I == 109] = 0   # erase background (background type 2)
-        I[I != 0] = 1     # everything else (paddles, ball) just set to 1
+        def crop(x):
+            return x[35:195]
+        
+        def downsample(x):
+            return x[::2,::2,0]
+        
+        def erase_background(x):
+            x[x == 144] = 0
+            x[x == 109] = 0
+            x[x != 0] = 1
+            return x
+        
+        I = erase_background(downsample(crop(I)))
         return I.astype(np.float).ravel()
     
+    def move(self, p):
+        random_value = np.random.uniform()
+        if random_value < p: return 2 # UP
+        else: return 3 # DOWN
+    
+    def discount_rewards(self, r):
+        ''' Return a discounted reward array from the original '''
+        running_add = 0
+        discounted_r = np.zeros_like(r)
+        for t in reversed(range(0, r.size)):
+          if r[t] != 0: running_add = 0 # reset the sum, since this was a game boundary (pong specific!)
+          running_add = running_add * self.gamma + r[t]
+          discounted_r[t] = running_add
+        return discounted_r
+    
+    def normalize_rewards(self, r):
+        ''' Normalize a reward array to force half choices being good/bad '''
+        r -= np.mean(r)
+        r /= np.std(r)
+        return r
+        
     def policy_forward(self, x:float):
+        ''' Returns the probability of taken an action and the hidden state '''
         h = np.dot(self.W1.T, x) 
         h[h<0] = 0 # ReLU 
         x = np.dot(self.W2.T, h) # Log probability of UP
         p = self.sigmoid(x)  # Squeeze logp [0-1]
         return p, h
     
-    def policy_backward(self, eph, epdlogp):
-        """ backward pass. (eph is array of intermediate hidden states) """
+    def policy_backward(self, epx, eph, epdlogp):
+        ''' Backward pass. Calculate the gradients '''
         dW2 = np.dot(eph.T, epdlogp).ravel()
         dh = np.outer(epdlogp, self.W2)
-        dh[eph <= 0] = 0 # backpro prelu
-        dW1 = np.dot(dh.T, eph)
+        dh[eph <= 0] = 0 
+        dW1 = np.dot(dh.T, epx)
         return {'W1':dW1, 'W2':dW2}
     
-    
+    def update_weights(self, g):
+        for layer_name in self.layers.keys():
+            g = self.g_dict[layer_name]
+            self.exp_g2[layer_name] = self.decay_rate * self.exp_g2[layer_name] + (1 - self.decay_rate) * g**2
+            self.layers[layer_name] += (self.learning_rate * g)/(np.sqrt(self.exp_g2[layer_name] + self.epsilon))
+            self.g_dict[layer_name] = np.zeros_like(self.layers[layer_name]) # reset batch gradient memory
+
+        
     
 HIDDEN = 200
 ROLLOUTS = 80 
@@ -63,8 +123,7 @@ h = np.dot(w1, x)
         
 
 
-
-
+# Torch Implementation
 import torch.nn as nn
 
 class AgentTorch():
--- 2_Deep_RL/2_Policy_Based_Methods/2_Proximal_Policy_Optimization/pong/pong.py
+++ 2_Deep_RL/2_Policy_Based_Methods/2_Proximal_Policy_Optimization/pong/pong.py
@@ -14,14 +14,11 @@ import numpy as np
 env = gym.make("Pong-v0")
 observation = env.reset() # This gets us the image
 
-prev_x = None # used in computing the difference frame
-xs,hs,dlogps,drs = [],[],[],[]
 
+prev_x = None 
 reward_sum = 0
 episode_number = 0
 running_reward = None
-
-render = False
 
 
 # Set Agent in the world
@@ -35,21 +32,78 @@ ROLLOUTS = 80
 BATCHSIZE = 10 # Update frequency (episodes)
 
 agent = Agent(D, HIDDEN)
+xs,hs,dlogps,drs = [],[],[],[] # Memory needed for RMSProp
+
 
 # Play Pong!
 # ----------
 
+render = True
 while True:
 
+    ep_steps = 0
+    agent.restart_gradients()
     if render: env.render()
     
     # Image differencing
-    cur_x = agent.prepro(observation)
+    cur_x = agent.preprocess(observation)
     x = cur_x - prev_x if prev_x is not None else np.zeros(D)
     prev_x = cur_x
     
     # Sampling and action based on our current policy
     aprob, h = agent.policy_forward(x)
-    action = 2 if np.random.uniform() < aprob else 3 
+    action = agent.move(aprob)
+    
+    # Keep the values for future RMSProp gradient computation 
+    xs.append(x) 
+    hs.append(h) 
+    fake_label = 1 if action == 2 else 0 
+    dlogps.append(fake_label - aprob) 
+
+    # Move
+    observation, reward, done, info = env.step(action)
+    reward_sum += reward
+    
+    drs.append(reward) # record reward 
+    
+    ep_steps += 1
+    
+    # If the game is finished:
+    if done:
+        
+        episode_number += 1
+        
+        # Wrap inputs, hidden states, action grandients and rewards
+        ep_x = np.vstack(xs)
+        ep_h = np.vstack(hs)
+        ep_dlogp = np.vstack(dlogps)
+        ep_r = np.vstack(drs)
+        
+        # Apply discount to the rewards and standardize
+        ep_r = agent.normalize_rewards(agent.discount_rewards(ep_r))
+        
+        # Plug-in the rewards to the gradient function
+        ep_dlogp *= ep_r
+        grad = agent.policy_backward(ep_x, ep_h, ep_dlogp)
+        
+        # Sum the gradient for use when we hit the batch size
+        agent.aggregate_gradients(grad)
+        
+        # Update rule if Batch size of examples
+        if episode_number % BATCHSIZE == 0:
+                agent.update_weights()
+
+        
+        xs,hs,dlogps,drs = [],[],[],[]  # Reset memory
+        observation = env.reset()       # Reset env
+        running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
+        print('resetting env. episode reward total was %f. running mean: %f' % (reward_sum, running_reward))
+        reward_sum = 0
+        prev_processed_observations = None
+        
+
+
     
     
+    
+
