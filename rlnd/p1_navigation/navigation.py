e350b519c83c4d2c4b94c4a8b22c641b36a7076f
PabloRR100 <pabloruizruiz10@gmail.com>
2018-11-28 16:36:52 +0100

    banana world training

--- rlnd/p1_navigation/navigation.py
+++ rlnd/p1_navigation/navigation.py
@@ -9,9 +9,11 @@ Navigation
 
 import torch
 import numpy as np
-from unityagents import UnityEnvironment
+from utils import timeit
+from collections import deque
 
 # Load World
+from unityagents import UnityEnvironment
 env = UnityEnvironment(file_name = 'Banana.app')
 
 # Load Brain (default)
@@ -37,22 +39,144 @@ Unity brain name: BananaBrain
         Vector Action descriptions: , , , 
 '''
 
+
+# World Parameters
+# ----------------
+action_size = brain.vector_action_space_size
+state_size = len(env.reset(train_mode=True)[brain_name].vector_observations[0])
+
+
 # Hyperparameters 
 # ---------------
 
-LR = 5e-4               # learning rate 
-GAMMA = 0.99            # discount factor
-BATCH_SIZE = 64         # minibatch size
+seed = 2018
 
-TAU = 1e-3              # for soft update of target parameters
-UPDATE_EVERY = 4        # how often to update the network
-BUFFER_SIZE = int(1e5)  # replay buffer size
+h_layers=[32,32]    # Hidden Layers
+LR = 5e-4           # Learning rate 
+BS = 64             # Minibatch size
+GAMMA = 0.99        # Discount factor
+    
+UE = 4              # How often to update the network
+TAU = 1e-3          # For soft update of target parameters
+BFS = int(1e5)      # Memory capacity
 
+
+EPISODES = 2000     # Training episodes
+TIMESTEPS = 1000    # Max # of timesteps per episode
+eps_0 = 1.0         # Initial exploration (epsilon)
+eps_F = 0.01        # Minimal exploration
+eps_decay = 0.995   # Exploration decay rate
 
 
 # Create Agent
 # ------------
 
-from dqn import DQAgent
-agent = DQAgent()
+from dqn import DQAgent, DoubleDQAgent
+agent = DQAgent(state_size, action_size, seed, h_layers, LR, BS, BFS, GAMMA, TAU, UE)
+agent = DoubleDQAgent(state_size, action_size, seed, h_layers, LR, BS, BFS, GAMMA, TAU, UE)
+
+agent
+
+
+# Training 
+# --------
+
+@timeit
+def train(EPISODES, TIMESTEPS, eps_0, eps_F, eps_decay):
+    eps = eps_0
+    episode_scores = list()
+    last_scores = deque(maxlen=100)
+    def monitor(e,s):
+        if e % 100 == 0: 
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(e, np.mean(s)), end="")
+        
+    def solved(e,s):
+        solved = np.mean(last_scores) >= 13.0
+        if solved:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}' \
+                  .format(e-100, np.mean(s)))
+            return True
+        return False
+    
+    # Run episodes
+    for e in range(EPISODES):
+        
+        # Reset the environment
+        score = 0
+        env_info = env.reset(train_mode=True)[brain_name]
+        state = env.reset(train_mode=True)[brain_name].vector_observations[0]
+        
+        # Run timesteps within the episodes
+        for t in range(TIMESTEPS):
+            
+            # Take an action, reach new state and observe respective reward
+            action = agent.act(state, eps).astype(int)
+            env_info = env.step(action)[brain_name]       
+            next_state = env_info.vector_observations[0]    # What's next state? 
+            reward = env_info.rewards[0]                    # What's the reward?
+            done = env_info.local_done[0]                   # Is it finished?
+            
+            # Advance
+            agent.step(state, action, reward, next_state, done)
+            state = next_state
+            score += reward
+            if done: break
+        
+        # Episode stats
+        last_scores.append(score)
+        episode_scores.append(score)
+        
+        # Decrease exploration (epsilon)
+        eps = max(eps_F, eps_decay*eps)
+        
+        # Monitoring
+        monitor(e, last_scores)
+        if solved(e,score):
+            torch.save(agent.qnetwork_local.state_dict(), 'checkpoint.pkl')
+        
+    return episode_scores
+    
+# We don't see agents while training
+env_info = env.reset(train_mode=False)[brain_name]
+state = env_info.vector_observations[0]
+
+scores = train(EPISODES, TIMESTEPS, eps_0, eps_F, eps_decay)
+
+
+# Training Analysis
+# -----------------
+
+import seaborn as sns
+sns.set('dark')
+
+
+# Observe Trained Agent on the Environment
+# ----------------------------------------
+
+# Load the brain
+agent = DQAgent
+agent.qnetwork_local.load_state_dict(torch.load('checkpoint.pth'))
+
+for i in range(5):
+    
+    env_info = env.reset(train_mode=False)[brain_name]
+    state = env_info.vector_observations[0]
+    
+    for j in range(200):
+        action = agent.act(state).astype(int)
+        env_info = env.step(action)[brain_name]
+        state = env_info.vector_observations[0]  
+        done = env_info.local_done[0]
+        if done:
+            break
+    
+    
+    
+        
+        
+    
+
+    
+ 
+
 
