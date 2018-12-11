2b26c7bab208dfa51d1938136c8d98e4699358d6
PabloRR100 <pabloruizruiz10@gmail.com>
2018-12-11 11:42:45 +0100

    common stuff

---
+++ rlnd/p3_collab-compet/main.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: pabloruizruiz
+Continuous Control - Unity Environment - The Reacher
+"""
+
+import time
+import torch
+import pickle
+import numpy as np
+#from utils import timer
+from collections import deque
+import matplotlib.pyplot as plt
+
+from agent import Agent
+from unityagents import UnityEnvironment
+from utils import timeit, count_parameters
+
+cuda = True if torch.cuda.is_available() else False
+gpus = True if torch.cuda.device_count() > 1 else False
+
+EPISODES = 300
+MAX_ITERS = 1000
+PRINT_EVERY = 100
+LEARN_PERIOD = 20
+NUM_SAMPLES = 10
+
+# ENV = 'Reacher.app'
+ENV = 'Reacher20.app'
+
+
+test = False
+if test:
+    
+    # Test World
+    # ----------
+    
+    env = UnityEnvironment(file_name=ENV)
+    
+    brain_name = env.brain_names[0]
+    brain = env.brains[brain_name]
+    
+    env_info = env.reset(train_mode=True)[brain_name]
+    num_agents = len(env_info.agents)
+    print('Number of Agents: ', num_agents)
+    
+    env.close()
+    
+    
+    # Test Agent
+    # ----------
+    
+    state_size, action_size = brain.vector_observation_space_size, brain.vector_action_space_size
+    agent = Agent(num_agents=num_agents, state_size=state_size, action_size=action_size)
+    
+    print('Capacity of the Actor (# of parameters): ', count_parameters(agent.actor_local))
+    print('Capacity of the Critic (# of parameters): ', count_parameters(agent.critic_local))
+
+        
+# Training
+# --------
+
+#@timeit
+def train(env):
+    ''' Trains on an environment '''
+    
+    global EPISODES
+    global MAX_ITERS
+    global PRINT_EVERY
+    global LEARN_PERIOD
+    global NUM_SAMPLES
+    
+    print('Loading environmnet...\n')
+    env = UnityEnvironment(file_name=ENV)
+    brain_name = env.brain_names[0]
+    brain = env.brains[brain_name]
+    env_info = env.reset(train_mode=True)[brain_name]
+    
+    
+    print('Loading agent...\n')
+    num_agents = len(env_info.agents)
+    state_size, action_size = brain.vector_observation_space_size, brain.vector_action_space_size
+    agent = Agent(num_agents=num_agents, state_size=state_size, action_size=action_size)
+    print('Capacity of the Actor (# of parameters): ', count_parameters(agent.actor_local))
+    print('Capacity of the Critic (# of parameters): ', count_parameters(agent.critic_local))
+
+    
+    last_100_mean = []
+    scores_global = []
+    scores_concur = deque(maxlen=PRINT_EVERY)
+    
+    try:
+        
+        print('Initializing training...\n')
+        for e in range(1, EPISODES+1):
+            
+            # Initialize Episode
+            scores = np.zeros(num_agents)
+            env_info = env.reset(train_mode=True)[brain_name]
+            states = env_info.vector_observations                  # get the current state (for each agent)
+            
+            agent.reset()
+            t0 = time.time()
+            
+            # Run episode maximum until MAX_ITERS
+            for i in range(MAX_ITERS):
+                
+                # Select an action for each Agent
+                actions = agent.act(states)
+                env_info = env.step(actions)[brain_name]          
+                
+                # Observe result of the action
+                next_states = env_info.vector_observations         
+                rewards = env_info.rewards                         
+                dones = env_info.local_done   
+                         
+                # Store score result
+                scores += env_info.rewards                         
+                
+                # Make a step on the environment 
+                for state, action, reward, next_state, done in zip(states, actions, rewards, next_states, dones):
+                    agent.step(state, action, reward, next_state, done)
+                
+                if i % PRINT_EVERY == 0:
+                    print('\rStep {}\tAverage Score: {:.2f}'.format(i, np.mean(scores)), end="")
+                    
+                # Learn from experiences on the Replay Buffer 
+                if i % LEARN_PERIOD == 0:
+                    for sample in range(NUM_SAMPLES):
+                        agent.sampleandlearn()
+                
+                # End of the episode if any of the agents is done 
+                if np.any(dones):                                  
+                    break
+                
+                # Roll over states to next time step
+                states = next_states                               
+                
+            # agent.sampleandlearn() 
+            
+            deltatime = time.time() - t0
+            
+            score = np.mean(scores)
+            scores_concur.append(score)
+            scores_global.append(score)
+            print('\rEpisode {}, Average last 100 scores: {:.2f}, Episode Duration: {:.2f}, \n'\
+                  .format(e, np.mean(scores_concur), deltatime))
+            
+            # If last 100 episodes average score is the best 100 average seen - Save Models
+            if np.mean(scores_concur) > last_100_mean:
+                torch.save(agent.actor_local.state_dict(), 'checkpoint_actor_{}.pth'.format(e))
+                torch.save(agent.critic_local.state_dict(), 'checkpoint_critic_{}.pth'.format(e))
+            
+            # Update current 100 mean            
+            last_100_mean = np.mean(scores_concur)
+        
+        print('Closing envionment...\n')
+        env.close()
+        return agent, scores_global
+    
+    # If errors, close environment 
+    except:
+        env.close()
+        print('There were some error wile training')
+        return None, None
+
+
+
+# Init Training
+# -------------
+
+agent, scores = train(ENV)
+with open('results.pickle', 'wb') as output:
+    pickle.dump(scores, output, protocol=pickle.HIGHEST_PROTOCOL)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.plot(np.arange(1, len(scores)+1), scores)
+plt.ylabel('Score')
+plt.xlabel('Episode #')
+plt.show()
+
+
+
+## Load Trained Agent
+## ------------------
+#
+#agent = Agent(state_size=state_size, action_size=action_size)
+#agent.actor_local.load_state_dict(torch.load('checkpoint_actor.pth'))
+#agent.critic_local.load_state_dict(torch.load('checkpoint_critic.pth'))
+#
+#with open('results.pickle', 'rb') as input:
+#    scores = pickle.load(input)
+#    
---
+++ rlnd/p3_collab-compet/report/README_Change.md
@@ -0,0 +1,107 @@
+
+# Continuous Control Project
+
+
+## Algorithm: Deep Deterministic Policy Gradient
+The RL algorithm used to solve this project is Deep Deterministic Policy Gradient, which was introduced by DeepMind in the publication ["Continuous control with deep reinforcement learning"][paper].  
+
+DDPG solves the problem of having a continuous action space.  
+DDPG uses the Actor-Critic framework, but it is considered by some researchers as an extension of DQN since uses the target network key idea. The critic is used to approximate the maximizer over the *q-values* of the next state, instead of being used as a learned baseline.   
+
+##### Actor
+Is used to approximate the optimal policy ***deterministically***. It always outputs the best believed action. Stochastic methods on the other hand want to learn a probability distribution over actions.  
+This actor is learning the *argmaxQ(s,a)* action.
+
+##### Critic
+It does the policy evaluation step. Therefore, it learns to evaluate the optimal action-value funtion by using the actors best believed actions.  
+
+##### Replay Buffer
+DDPG uses a Replay Buffer to decorrelate the tuples of experiences obtained sequentially.  
+However, the uptades are slightly different than how the Replay Buffer works on other algorithms like DQN.  
+
+This time, they are called ***soft updates***. 
+This strategy consists on *slowly blending* the Regular Network weights into the Target weigths. Every timestep, the Target is 99% weights of its own and 1% the weights of the Regular Network.
+
+This is opposed to the DQN implementation where the Target Network is updated every 10000 steps by coping the weights of the Regular Network into the Target Network. The Target Network then is fixed for a long time and then it sufferts a big update, intead of the slow blending mentioned above.
+
+
+
+## Training Process
+
+```python
+# Init Training
+# -------------
+
+agent, scores = train(ENV)
+results = dict(agent=agent, scores=scores)
+```
+
+    Loading environmnet...
+    
+
+
+    INFO:unityagents:
+    'Academy' started successfully!
+    Unity Academy name: Academy
+            Number of Brains: 1
+            Number of External Brains : 1
+            Lesson number : 0
+            Reset Parameters :
+    		goal_speed -> 1.0
+    		goal_size -> 5.0
+    Unity brain name: ReacherBrain
+            Number of Visual Observations (per agent): 0
+            Vector Observation space type: continuous
+            Vector Observation space size (per agent): 33
+            Number of stacked Vector Observation: 1
+            Vector Action space type: continuous
+            Vector Action space size (per agent): 4
+            Vector Action descriptions: , , , 
+
+
+    Loading agenst...
+    
+    Number of Agents loaded:  20
+    Capacity of the Actor (# of parameters):  162436
+    Capacity of the Critic (# of parameters):  108545
+    Initializing training...
+    
+    Episode 1, Average last 100 scores: 0.01, Episode Duration: 561.76, 
+    
+    Episode 2, Average last 100 scores: 0.01, Episode Duration: 621.55, 
+    
+    Episode 3, Average last 100 scores: 0.01, Episode Duration: 631.52, 
+    
+    Episode 4, Average last 100 scores: 0.01, Episode Duration: 648.75, 
+    
+    Episode 5, Average last 100 scores: 0.01, Episode Duration: 655.82, 
+    
+    Episode 6, Average last 100 scores: 0.01, Episode Duration: 674.15, 
+    
+    Step 500	Average Score: 0.02
+
+**I stopped the training here since it was not improving at all and I only have 4 GPU hours left for finish this project and next one**
+
+## Results 
+
+```python
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.plot(np.arange(1, len(scores)+1), scores)
+plt.ylabel('Score')
+plt.xlabel('Episode #')
+plt.show()
+```
+
+This code will plot the evolution of the scores once the training is completed
+![png](output_9_1.png)
+
+
+## Future Improvements  
+
+1 - Tuning of the hyperparameters to speed up training and increase performance.  
+2 - Try different algorithms:
+  [Continuous Deep Q-Learning with Model-based Acceleration][2] 
+
+[paper]: https://arxiv.org/abs/1509.02971
+[2]: https://arxiv.org/abs/1603.00748
